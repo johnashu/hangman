91c0dee1977e34190ab507ed03e29281f00c5b45
johnashu <johnashu1@hotmail.com>
2017-10-11 15:17:12 +0200

    printing on one line

--- hangman.py
+++ hangman.py
@@ -24,37 +24,27 @@ def play():
             for f in found:
                 for i in range(len(progress)):                
                     progress[f] = guess
-            print("\n\nNice One, '%s' Is A Correct Letter!!.." % (guess))
-            print("\nCurrent Progress: ", ''.join(progress))
-            print("\nLetters Used: ", ' | '.join(letters_used), '\n\n')
+            print("\n\nNice One, '%s' Is A Correct Letter!!.." % (guess), "\nCurrent Progress: ", ''.join(progress), "\nLetters Used: ", ' | '.join(letters_used), '\n\n')
         elif guess in letters_used:
-            print("\n\nYou Have Already Picked That Letter!!!")
-            print("\nCheck The Letters and Try Again!!", ' | '.join(letters_used))
-            print("\nCurrent Progress: ", ''.join(progress), '\n\n')
+            print("\n\nYou Have Already Picked That Letter!!!\nCheck The Letters and Try Again!!", ' | '.join(letters_used), "\nCurrent Progress: ", ''.join(progress), '\n\n')
         elif guess not in hangword and guess not in letters_used:
             incorrect += 1
-            print("\n\nThat Letter Is Not In The Word, You Have used %d Guesses Out of 7" % (incorrect))
             letters_used.append(guess)
-            print("\nCheck The Letters and Try Again!!", ' | '.join(letters_used))
-            print("\nCurrent Progress: ", ''.join(progress), '\n\n')    
+            print("\n\nThat Letter Is Not In The Word, You Have used %d Guesses Out of 7" % (incorrect), "\nCheck The Letters and Try Again!!", ' | '.join(letters_used), "\nCurrent Progress: ", ''.join(progress), '\n\n')    
     if ''.join(progress) == hangword:
-        print("\nCongratulations You have Saved the Hangman!!! :)")
-        print("\nYou used %d Guesses" % (incorrect))
-        print("\nThe Answer is: ", hangword)
-        print("You Saved THe Hangman THis GAme In %f seconds" % (time.time() - start_time))  
+        print("\nCongratulations You have Saved the Hangman!!! :)\nYou used %d Guesses" % (incorrect), "\nThe Answer is: ", hangword, "You Saved THe Hangman THis GAme In %f seconds" % (time.time() - start_time)) 
         new_game = input("Would You Like To Play Again? y/n?: ")   
         if new_game == 'y':
             play()
         else:
             quit()
     else:
-        print("\nSorry You Failed, The Hangman Is DeeD!!!")
-        print("\nThe Answer is: ", hangword, '\n')   
-        print("It Took You In %f Seconds To Send The Man To his Death!" % (time.time() - start_time), '\n\n')
+        print("\nSorry You Failed, The Hangman Is DeeD!!! \nThe Answer is: ", hangword, '\n', "It Took You In %f Seconds To Send The Man To his Death!" % (time.time() - start_time), '\n\n')
         new_game = input("Would You Like To Play Again? y/n?: ")   
         if new_game == 'y':
             play()
         else:
             quit()       
-play()
 
+
+play()
